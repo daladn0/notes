9d956e3c5f5bf4eb0a3f1b6c289c472adc093f82
daladn0 <48182958+daladn0@users.noreply.github.com>
2020-05-03 18:45:54 +0300

    Update Заимствовании методов.js

--- Заимствовании методов.js
+++ Заимствовании методов.js
@@ -12,3 +12,19 @@ obj.forEach = Array.prototype.forEach;
 console.log(obj.join(', '));
 console.log('------------');
 obj.forEach( item => console.log(item) );
+
+// Мы можем унаследовать все методы сразу установив __proto__ = Array.prototype;
+
+let obj = {
+    0: 'Hello',
+    1: 'world!',
+    length: 2,
+};
+
+obj.__proto__ = Array.prototype;
+
+console.log(obj.join(', '));
+
+obj.forEach( item => console.log(item) );
+
+console.log( obj.map( item => item.length ) );
